fa54f368b1aac5faf5629914f632af84f20a8b4f
Crowdbotics <team@crowdbotics.com>
2024-02-05 03:52:35 +0000

    Initial commit

---
+++ backend/holder_sitemonitor_45843/wsgi.py
@@ -0,0 +1,18 @@
+"""
+WSGI config for holder_sitemonitor_45843 project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault(
+    "DJANGO_SETTINGS_MODULE", "holder_sitemonitor_45843.settings"
+)
+
+application = get_wsgi_application()
---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "holder-sitemonitor-45843",
+  url: "https://holder-sitemonitor-45843.botics.co",
+  api: "https://holder-sitemonitor-45843.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
